84a116311de71aca2ae3d85afd95ea31265a5e7c
Misha <borisov.mikhail90@ya.ru>
2022-10-18 02:00:07 +0300

    feat: schema.sql

---
+++ server/src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+create table if not exists users (
+    id bigint generated always as identity primary key ,
+    name varchar(100),
+    email varchar(100) unique ,
+    created timestamp
+);
+
+create table if not exists locations(
+    id bigint generated always as identity primary key ,
+    lat double precision,
+    lon double precision,
+    description varchar(200),
+    unique (lat, lon)
+);
+
+create table if not exists category(
+    id int generated always as identity primary key ,
+    name varchar(100)
+);
+
+create table if not exists events (
+    id bigint generated always as identity primary key ,
+    title varchar(250),
+    annotation varchar(250),
+    description varchar(1000),
+    initiator bigint references users,
+    category int references category,
+    eventDate timestamp,
+    location bigint references locations,
+    paid boolean,
+    partition_limit int,
+    moderation boolean,
+    state int
+);
+
+create table if not exists participation_requests (
+    id bigint generated always as identity primary key ,
+    created timestamp,
+    requestor bigint references users,
+    event bigint references events,
+    status int
+);
+
+create table if not exists selections (
+    id bigint generated always as identity primary key ,
+    name varchar(100)
+);
+
+create table if not exists events_selections (
+    selection_id bigint references selections,
+    event_id bigint references events,
+    unique (selection_id, event_id)
+);
