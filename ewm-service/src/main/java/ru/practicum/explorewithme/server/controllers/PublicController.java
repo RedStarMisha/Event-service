1d3d17cb94053baac0efcde3e4a5724a1d4e7e0b
RedstarMisha <borisov.mikhail90@yandex.ru>
2022-11-02 11:34:43 +0300

    feat:Сервис статистики почти готов

--- ewm-service/src/main/java/ru/practicum/explorewithme/server/controllers/PublicController.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/server/controllers/PublicController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import ru.practicum.explorewithme.models.category.CategoryDto;
 import ru.practicum.explorewithme.models.compilation.CompilationDto;
 import ru.practicum.explorewithme.models.event.EventFullDto;
